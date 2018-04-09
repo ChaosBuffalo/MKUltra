6216cd6734f6d40da3a093148d5a7ffbc6e1adae
Lee Harrison <ralekdev@gmail.com>
2018-04-08 22:19:37 -0700

    Targeting: Mark controlled entities friendly if the owner is friendly

--- src/main/java/com/chaosbuffalo/mkultra/effects/Targeting.java
+++ src/main/java/com/chaosbuffalo/mkultra/effects/Targeting.java
@@ -1,9 +1,11 @@
 package com.chaosbuffalo.mkultra.effects;
 
+import com.chaosbuffalo.mkultra.log.Log;
 import com.google.common.collect.Sets;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.IEntityOwnable;
 import net.minecraft.entity.monster.EntityIronGolem;
 import net.minecraft.entity.passive.EntityVillager;
 import net.minecraft.entity.player.EntityPlayer;
@@ -68,20 +70,75 @@ public class Targeting {
         return false;
     }
 
-    private static boolean isValidFriendly(Entity caster, EntityLivingBase target) {
+    private static boolean isSameTeam(Entity caster, Entity target) {
+        Team myTeam = caster.getTeam();
+        Team otherTeam = target.getTeam();
+        return myTeam != null && otherTeam != null && myTeam.isSameTeam(otherTeam);
+    }
+
+    private static boolean isFriendlyPlayer(Entity caster, Entity target) {
+
+        if (caster.isEntityEqual(target))
+            return true;
+
+        // Currently the only friendly player check is if the caster and target are on the same team
+        if (isSameTeam(caster, target))
+            return true;
+
+        return false;
+    }
+
+    private static boolean isFriendlyPlayerControlled(Entity caster, Entity target) {
+        Entity controller = target.getControllingPassenger();
+        if (controller instanceof EntityPlayer) {
+            return isValidFriendly(caster, controller);
+        }
+
+        if (target instanceof IEntityOwnable) {
+            Entity owner = ((IEntityOwnable)target).getOwner();
+
+            if (owner != null) {
+                return isValidFriendly(caster, owner);
+            }
+            else {
+                Log.info("isFriendlyPlayerControlled: owner was null!");
+            }
+        }
+
+        return false;
+    }
+
+
+    private static boolean isValidFriendly(Entity caster, Entity target) {
 
         // Always friendly with ourselves
         if (caster.isEntityEqual(target)) {
             return true;
         }
 
-        if (target instanceof EntityPlayer) {
-            Team myTeam = caster.getTeam();
-            Team otherTeam = target.getTeam();
-            return myTeam != null && otherTeam != null && myTeam.isSameTeam(otherTeam);
-        } else {
-            return isValidFriendlyCreature(caster, target) || isRegisteredFriendly(target);
+        // Always friendly with whitelisted entities
+        if (isRegisteredFriendly(target)) {
+            return true;
         }
+
+        // Always friendly with entities on the same team
+        if (isSameTeam(caster, target)) {
+            return true;
+        }
+
+        if (isFriendlyPlayer(caster, target))
+            return true;
+
+        if (isFriendlyPlayerControlled(caster, target))
+            return true;
+
+        if (target instanceof EntityLivingBase) {
+            EntityLivingBase targetLiving = (EntityLivingBase) target;
+            if (isValidFriendlyCreature(caster, targetLiving))
+                return true;
+        }
+
+        return false;
     }
 
     private static boolean isValidEnemy(Entity caster, EntityLivingBase target) {
