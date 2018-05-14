4acf0b481937e161a1228d9bee2bf2a3147eeafd
Lee Harrison <ralekdev@gmail.com>
2018-05-13 21:59:43 -0700

    RayTraceUtils: Generic-ify some methods to allow more specific class filtering

--- src/main/java/com/chaosbuffalo/mkultra/utils/RayTraceUtils.java
+++ src/main/java/com/chaosbuffalo/mkultra/utils/RayTraceUtils.java
@@ -1,13 +1,3 @@
-/*
- * VariousUtils.java (VariousUtils)
- * Package minersbasic.internal
- *
- * Project: Miners Basic
- *
- * Author: _Bedrock_Miner_ (minerbedrock@gmail.com)
- *
- * Last Modified: 15.04.2015
- */
 package com.chaosbuffalo.mkultra.utils;
 
 import net.minecraft.entity.Entity;
@@ -24,10 +14,6 @@ import java.util.function.Predicate;
 
 public class RayTraceUtils {
 
-    public static RayTraceResult getLookingAt(final Entity mainEntity, double distance) {
-        return getLookingAt(mainEntity, distance, e -> e != mainEntity);
-    }
-
     private static Predicate<Entity> defaultFilter = e -> EntitySelectors.IS_ALIVE.apply(e) && EntitySelectors.NOT_SPECTATING.apply(e);
 
 
@@ -41,19 +27,20 @@ public class RayTraceUtils {
         return vec.crossProduct(cVec);
     }
 
-    public static List<Entity> getEntitiesInLine(final Entity mainEntity, Vec3d from, Vec3d to, Vec3d expansion,
-                                                 float growth, final Predicate<Entity> filter) {
-        Predicate<Entity> predicate = input -> defaultFilter.test(input) && filter.test(input);
+    public static <E extends Entity> List<E> getEntitiesInLine(Class<E> clazz, final Entity mainEntity,
+                                                               Vec3d from, Vec3d to, Vec3d expansion,
+                                                               float growth, final Predicate<E> filter) {
+        Predicate<E> predicate = e -> defaultFilter.test(e) && filter.test(e);
         AxisAlignedBB bb = new AxisAlignedBB(new BlockPos(from), new BlockPos(to))
                 .expand(expansion.x, expansion.y, expansion.z)
                 .grow(growth);
-        List<Entity> entities = mainEntity.getEntityWorld().getEntitiesWithinAABB(Entity.class, bb, predicate::test);
+        List<E> entities = mainEntity.getEntityWorld().getEntitiesWithinAABB(clazz, bb, predicate::test);
         return entities;
     }
 
-    public static RayTraceResult getLookingAt(final Entity mainEntity, double distance, final Predicate<Entity> entityPredicate) {
+    public static <E extends Entity> RayTraceResult getLookingAt(Class<E> clazz, final Entity mainEntity, double distance, final Predicate<E> entityPredicate) {
 
-        Predicate<Entity> finalFilter = e -> e != mainEntity &&
+        Predicate<E> finalFilter = e -> e != mainEntity &&
                 defaultFilter.test(e) &&
                 e.canBeCollidedWith() &&
                 entityPredicate.test(e);
@@ -64,7 +51,7 @@ public class RayTraceUtils {
             Vec3d look = mainEntity.getLookVec().scale(distance);
             Vec3d from = mainEntity.getPositionVector().addVector(0, mainEntity.getEyeHeight(), 0);
             Vec3d to = from.add(look);
-            position = rayTraceBlocksAndEntities(mainEntity.world, from, to, false, finalFilter);
+            position = rayTraceBlocksAndEntities(clazz, mainEntity.world, from, to, false, finalFilter);
         }
         return position;
     }
@@ -73,11 +60,19 @@ public class RayTraceUtils {
         return world.rayTraceBlocks(from, to, stopOnLiquid, true, false);
     }
 
-
     public static RayTraceResult rayTraceEntities(World world, Vec3d from, Vec3d to, Vec3d aaExpansion, float aaGrowth,
                                                   float entityExpansion, final Predicate<Entity> filter) {
+        return rayTraceEntities(Entity.class, world, from, to, aaExpansion, aaGrowth, entityExpansion, filter);
+    }
 
-        Predicate<Entity> predicate = input -> defaultFilter.test(input) && filter.test(input);
+    public static <E extends Entity> RayTraceResult rayTraceEntities(Class<E> clazz, World world,
+                                                                     Vec3d from, Vec3d to,
+                                                                     Vec3d aaExpansion,
+                                                                     float aaGrowth,
+                                                                     float entityExpansion,
+                                                                     final Predicate<E> filter) {
+
+        Predicate<E> predicate = input -> defaultFilter.test(input) && filter.test(input);
 
         Entity nearest = null;
         double distance = 0;
@@ -85,7 +80,7 @@ public class RayTraceUtils {
         AxisAlignedBB bb = new AxisAlignedBB(new BlockPos(from), new BlockPos(to))
                 .expand(aaExpansion.x, aaExpansion.y, aaExpansion.z)
                 .grow(aaGrowth);
-        List<Entity> entities = world.getEntitiesWithinAABB(Entity.class, bb, predicate::test);
+        List<E> entities = world.getEntitiesWithinAABB(clazz, bb, predicate::test);
         for (Entity entity : entities) {
             AxisAlignedBB entityBB = entity.getEntityBoundingBox().expand(entityExpansion, entityExpansion, entityExpansion);
             RayTraceResult intercept = entityBB.calculateIntercept(from, to);
@@ -102,12 +97,12 @@ public class RayTraceUtils {
         return null;
     }
 
-    private static RayTraceResult rayTraceBlocksAndEntities(World world, Vec3d from, Vec3d to, boolean stopOnLiquid,
-                                                            final Predicate<Entity> entityFilter) {
+    private static <E extends Entity> RayTraceResult rayTraceBlocksAndEntities(Class<E> clazz, World world, Vec3d from, Vec3d to, boolean stopOnLiquid,
+                                                            final Predicate<E> entityFilter) {
         RayTraceResult block = rayTraceBlocks(world, from, to, stopOnLiquid);
         if (block != null)
             to = block.hitVec;
-        RayTraceResult entity = rayTraceEntities(world, from, to, new Vec3d(0, 0, 0), 0.5f, 0.5f, entityFilter);
+        RayTraceResult entity = rayTraceEntities(clazz, world, from, to, Vec3d.ZERO, 0.5f, 0.5f, entityFilter);
 
         if (block == null) {
             if (entity == null) {
