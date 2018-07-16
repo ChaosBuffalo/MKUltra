c8277bb54fa6bffde50325681ba9df4bb5254b42
Lee Harrison <ralekdev@gmail.com>
2018-07-15 19:41:39 -0700

    Make AbilityTrackerServer a private class on AbilityTracker

--- src/main/java/com/chaosbuffalo/mkultra/core/AbilityTracker.java
+++ src/main/java/com/chaosbuffalo/mkultra/core/AbilityTracker.java
@@ -1,6 +1,10 @@
 package com.chaosbuffalo.mkultra.core;
 
+import com.chaosbuffalo.mkultra.MKUltra;
+import com.chaosbuffalo.mkultra.network.packets.server.AbilityCooldownPacket;
 import com.google.common.collect.Maps;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.util.math.MathHelper;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -80,6 +84,34 @@ public class AbilityTracker {
         private Cooldown(int createTicksIn, int expireTicksIn) {
             this.createTicks = createTicksIn;
             this.expireTicks = expireTicksIn;
+        }
+    }
+
+    static class AbilityTrackerServer extends AbilityTracker {
+
+        private EntityPlayerMP player;
+
+        public AbilityTrackerServer(EntityPlayerMP player) {
+            this.player = player;
+        }
+
+        protected void notifyOnSet(PlayerAbilityInfo itemIn, int ticksIn) {
+            super.notifyOnSet(itemIn, ticksIn);
+            MKUltra.packetHandler.sendTo(new AbilityCooldownPacket(itemIn.id, ticksIn), player);
+        }
+
+        protected void notifyOnRemove(PlayerAbilityInfo itemIn) {
+            super.notifyOnRemove(itemIn);
+            MKUltra.packetHandler.sendTo(new AbilityCooldownPacket(itemIn.id, 0), player);
+        }
+    }
+
+    public static AbilityTracker getTracker(EntityPlayer player) {
+        if (player instanceof EntityPlayerMP) {
+            return new AbilityTrackerServer((EntityPlayerMP) player);
+        }
+        else {
+            return new AbilityTracker();
         }
     }
 }
