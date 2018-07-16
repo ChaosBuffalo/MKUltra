f06f4871d348af24168434163abfc882d4878dbd
Kovak <kovac1066@gmail.com>
2018-07-15 20:18:12 -0700

    switch to crit message packet

---
+++ src/main/java/com/chaosbuffalo/mkultra/network/packets/client/CritMessagePacket.java
@@ -0,0 +1,161 @@
+package com.chaosbuffalo.mkultra.network.packets.client;
+
+import com.chaosbuffalo.mkultra.core.BaseAbility;
+import com.chaosbuffalo.mkultra.core.ClassData;
+import com.chaosbuffalo.mkultra.network.MessageHandler;
+import com.chaosbuffalo.mkultra.utils.ServerUtils;
+import io.netty.buffer.ByteBuf;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.network.PacketBuffer;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.Style;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
+import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
+
+import java.util.UUID;
+
+/**
+ * Created by Jacob on 7/15/2018.
+ */
+public class CritMessagePacket implements IMessage {
+    public enum CritType{
+        INDIRECT_MAGIC_CRIT,
+        MELEE_CRIT,
+        SPELL_CRIT,
+        INDIRECT_CRIT,
+    }
+    private int targetId;
+    private UUID sourceUUID;
+    private ResourceLocation abilityName;
+    private float critDamage;
+    private CritType type;
+
+    public CritMessagePacket() {
+    }
+
+    public CritMessagePacket(int targetId, UUID sourceUUID, float critDamage, CritType type) {
+        this.targetId = targetId;
+        this.sourceUUID = sourceUUID;
+        this.critDamage = critDamage;
+        this.type = type;
+    }
+
+    public CritMessagePacket(int targetId, UUID sourceUUID, float critDamage, ResourceLocation abilityName) {
+        this.targetId = targetId;
+        this.sourceUUID = sourceUUID;
+        this.critDamage = critDamage;
+        this.type = CritType.SPELL_CRIT;
+        this.abilityName = abilityName;
+    }
+
+    @Override
+    public void fromBytes(ByteBuf buf) {
+        PacketBuffer pb = new PacketBuffer(buf);
+        this.targetId = pb.readInt();
+        this.sourceUUID = pb.readUniqueId();
+        this.critDamage = pb.readFloat();
+        this.type = CritType.values()[pb.readInt()];
+        if (type == CritType.SPELL_CRIT){
+            this.abilityName = pb.readResourceLocation();
+        }
+    }
+
+    @Override
+    public void toBytes(ByteBuf buf) {
+        PacketBuffer pb = new PacketBuffer(buf);
+        pb.writeInt(targetId);
+        pb.writeUniqueId(sourceUUID);
+        pb.writeFloat(critDamage);
+        pb.writeInt(type.ordinal());
+        if (type == CritType.SPELL_CRIT){
+            pb.writeResourceLocation(this.abilityName);
+        }
+    }
+
+    // ========================================================================
+
+    public static class Handler extends MessageHandler.Server<CritMessagePacket> {
+
+
+        @Override
+        public IMessage handleServerMessage(final EntityPlayer player,
+                                            final CritMessagePacket msg,
+                                            MessageContext ctx) {
+            ServerUtils.addScheduledTask(() -> {
+                Style messageStyle = new Style();
+                boolean isSelf = player.getUniqueID().equals(msg.sourceUUID);
+                EntityPlayer playerSource = player.getEntityWorld().getPlayerEntityByUUID(msg.sourceUUID);
+                Entity target = player.getEntityWorld().getEntityByID(msg.targetId);
+                if (target == null || playerSource == null){
+                    return;
+                }
+                switch (msg.type){
+                    case MELEE_CRIT:
+                    case INDIRECT_CRIT:
+                        messageStyle.setColor(
+                                msg.type == CritType.MELEE_CRIT ? TextFormatting.DARK_RED : TextFormatting.GOLD
+                        );
+                        if (isSelf){
+                            player.sendMessage(new TextComponentString(
+                                    String.format("You just crit %s with %s for %s",
+                                            target.getDisplayName().getUnformattedText(),
+                                            playerSource.getHeldItemMainhand().getDisplayName(),
+                                            Float.toString(msg.critDamage)))
+                                    .setStyle(messageStyle));
+                        } else {
+                            player.sendMessage(new TextComponentString(
+                                    String.format("%s just crit %s with %s for %s",
+                                            playerSource.getDisplayName().getUnformattedText(),
+                                            target.getDisplayName().getUnformattedText(),
+                                            playerSource.getHeldItemMainhand().getDisplayName(),
+                                            Float.toString(msg.critDamage))
+                            ).setStyle(messageStyle));
+                        }
+                    case INDIRECT_MAGIC_CRIT:
+                        messageStyle.setColor(TextFormatting.BLUE);
+                        if (isSelf){
+                            player.sendMessage(new TextComponentString(
+                                    String.format("Your magic spell just crit %s for %s",
+                                            target.getDisplayName().getUnformattedText(),
+                                            Float.toString(msg.critDamage)))
+                                    .setStyle(messageStyle));
+                        } else {
+                            player.sendMessage(new TextComponentString(
+                                    String.format("%s's magic spell just crit %s for %s",
+                                            playerSource.getDisplayName().getUnformattedText(),
+                                            target.getDisplayName().getUnformattedText(),
+                                            Float.toString(msg.critDamage)))
+                                    .setStyle(messageStyle));
+                        }
+                    case SPELL_CRIT:
+                        messageStyle.setColor(TextFormatting.AQUA);
+                        BaseAbility ability = ClassData.getAbility(msg.abilityName);
+                        if (isSelf) {
+                            player.sendMessage(new TextComponentString(
+                                    String.format("Your %s spell just crit %s for %s",
+                                            ability.getAbilityName(),
+                                            target.getDisplayName().getUnformattedText(),
+                                            Float.toString(msg.critDamage)))
+                                    .setStyle(messageStyle)
+                            );
+
+                        } else {
+                            player.sendMessage(new TextComponentString(
+                                    String.format("%s's %s spell just crit %s for %s",
+                                            playerSource.getDisplayName().getUnformattedText(),
+                                            ability.getAbilityName(),
+                                            target.getDisplayName().getUnformattedText(),
+                                            Float.toString(msg.critDamage)))
+                                    .setStyle(messageStyle)
+                            );
+                        }
+                }
+            });
+            return null;
+        }
+
+    }
+}
