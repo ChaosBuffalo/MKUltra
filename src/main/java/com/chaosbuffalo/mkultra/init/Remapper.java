3cba1d0788545cfd2ca10954e0a4ca03b0d06927
Lee Harrison <ralekdev@gmail.com>
2018-07-22 17:41:27 -0700

    Rename bonedLeather* to boned_leather*

--- src/main/java/com/chaosbuffalo/mkultra/init/Remapper.java
+++ src/main/java/com/chaosbuffalo/mkultra/init/Remapper.java
@@ -2,6 +2,7 @@ package com.chaosbuffalo.mkultra.init;
 
 import com.chaosbuffalo.mkultra.MKUltra;
 import com.chaosbuffalo.mkultra.log.Log;
+import com.google.common.collect.Maps;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.util.ResourceLocation;
@@ -12,14 +13,22 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
 
+import java.util.Map;
+
 @Mod.EventBusSubscriber
 public class Remapper {
 
-    static final String MKX_MOD_ID = "mkultrax";
+    private static final String MKX_MOD_ID = "mkultrax";
 
+    private static Map<String, String> internalReplacements = Maps.newHashMap();
 
-    private static ResourceLocation mkxRes(String path) {
-        return new ResourceLocation(MKX_MOD_ID, path);
+    static {
+        internalReplacements.put("sunicon", "sun_icon");
+        internalReplacements.put("bonedleather", "boned_leather");
+        internalReplacements.put("bonedleatherchestplate", "boned_leather_chestplate");
+        internalReplacements.put("bonedleatherhelmet", "boned_leather_helmet");
+        internalReplacements.put("bonedleatherleggings", "boned_leather_leggings");
+        internalReplacements.put("bonedleatherboots", "boned_leather_boots");
     }
 
     private static boolean tryRemapToMKX(RegistryEvent.MissingMappings.Mapping<Item> entry) {
@@ -29,13 +38,9 @@ public class Remapper {
             return false;
         }
 
-        ResourceLocation mkxPath = mkxRes(entry.key.getResourcePath());
-        Item newItem = ForgeRegistries.ITEMS.getValue(mkxPath);
+        ResourceLocation mkxPath = new ResourceLocation(MKX_MOD_ID, entry.key.getResourcePath());
 
-        Log.info("Remapping %s to mkx %s: %b", entry.key.toString(), mkxPath.toString(), newItem != null);
-
-        if (newItem != null) {
-            entry.remap(newItem);
+        if (tryRemap(entry, mkxPath)) {
             return true;
         }
         else if (!Loader.isModLoaded(MKX_MOD_ID)) {
@@ -51,26 +56,38 @@ public class Remapper {
         return false;
     }
 
+    static boolean tryRemap(RegistryEvent.MissingMappings.Mapping<Item> entry, ResourceLocation newKey) {
+        Item replItem = ForgeRegistries.ITEMS.getValue(newKey);
+        Log.info("Remapping %s to %s: %b", entry.key.toString(), newKey.toString(), replItem != null);
+        if (replItem != null) {
+            entry.remap(replItem);
+            return true;
+        }
+        return false;
+    }
+
+
     @SubscribeEvent
     public static void missingItemMapping(RegistryEvent.MissingMappings<Item> event) {
         for (RegistryEvent.MissingMappings.Mapping<Item> entry : event.getAllMappings()) {
             Log.info("Remapping request for Item %s", entry.key.toString());
 
             if (entry.key.getResourceDomain().equals(MKUltra.MODID)) {
-                if (entry.key.getResourcePath().endsWith("spear")) {
+                String key = entry.key.getResourcePath();
+                if (key.endsWith("spear")) {
                     Log.info("Ignoring spear %s", entry.key.toString());
                     entry.ignore();
                     continue;
                 }
 
-                // sunicon -> sun_icon
-                if (entry.key.getResourcePath().equals("sunicon")) {
-                    entry.remap(ModItems.sun_icon);
-                    continue;
+                if (internalReplacements.containsKey(key)) {
+                    ResourceLocation newKey = new ResourceLocation(MKUltra.MODID, internalReplacements.get(key));
+                    if (tryRemap(entry, newKey)) {
+                        continue;
+                    }
                 }
-            }
 
-            if (entry.key.getResourceDomain().equals("minekampf")) {
+            } else if (entry.key.getResourceDomain().equals("minekampf")) {
                 Log.info("Removing old MK Item %s", entry.key.toString());
                 entry.ignore();
                 continue;
