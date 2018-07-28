257b525145a531dae09f27d910095bfe6087baf7
Lee Harrison <ralekdev@gmail.com>
2018-07-28 14:31:32 -0700

    Make ItemCriticalStats a private inner class

--- src/main/java/com/chaosbuffalo/mkultra/utils/ItemUtils.java
+++ src/main/java/com/chaosbuffalo/mkultra/utils/ItemUtils.java
@@ -80,4 +80,28 @@ public class ItemUtils {
         return DEFAULT_CRIT_DAMAGE;
 
     }
+
+    private static class ItemCriticalStats implements Comparable<ItemCriticalStats> {
+
+        public final Class<? extends Item> item;
+
+        public final int priority;
+
+        public final float chance;
+
+        public final float damageMultiplier;
+
+        public ItemCriticalStats(Class<? extends Item> itemClass, int priorityIn, float chanceIn, float damageMult){
+            item = itemClass;
+            priority = priorityIn;
+            chance = chanceIn;
+            damageMultiplier = damageMult;
+        }
+
+        @Override
+        public int compareTo(ItemCriticalStats o) {
+            return o.priority - priority;
+        }
+
+    }
 }
