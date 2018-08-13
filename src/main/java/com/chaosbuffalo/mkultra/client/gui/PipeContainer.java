4cfb02259355045970808db5a6d9a385c5e4c8d6
Kovak <kovac1066@gmail.com>
2018-08-12 23:54:11 -0700

    fix pipe container not shift clicking

--- src/main/java/com/chaosbuffalo/mkultra/client/gui/PipeContainer.java
+++ src/main/java/com/chaosbuffalo/mkultra/client/gui/PipeContainer.java
@@ -1,8 +1,12 @@
 package com.chaosbuffalo.mkultra.client.gui;
 
+import com.chaosbuffalo.mkultra.utils.SmokeUtils;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
 import net.minecraft.inventory.Container;
 import net.minecraft.inventory.Slot;
+import net.minecraft.item.EnumDyeColor;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.items.IItemHandler;
 
 /**
@@ -42,5 +46,68 @@ public class PipeContainer extends Container {
     @Override
     public boolean canInteractWith( EntityPlayer p ) {
         return true;
+    }
+
+    @Override
+    public ItemStack transferStackInSlot(EntityPlayer playerIn, int index)
+    {
+        ItemStack itemstack = ItemStack.EMPTY;
+        Slot slot = this.inventorySlots.get(index);
+
+        if (slot != null && slot.getHasStack())
+        {
+            ItemStack itemstack1 = slot.getStack();
+            itemstack = itemstack1.copy();
+
+            if (index == 0)
+            {
+                if (!this.mergeItemStack(itemstack1, 1, 37, true))
+                {
+                    return ItemStack.EMPTY;
+                }
+            }
+            else if (SmokeUtils.isSmokeable(itemstack1))
+            {
+                if (!this.mergeItemStack(itemstack1, 0, 1, true))
+                {
+                    return ItemStack.EMPTY;
+                }
+            }
+            else
+            {
+                if (((Slot)this.inventorySlots.get(0)).getHasStack() || !((Slot)this.inventorySlots.get(0)).isItemValid(itemstack1))
+                {
+                    return ItemStack.EMPTY;
+                }
+
+                if (itemstack1.hasTagCompound())// Forge: Fix MC-17431
+                {
+                    ((Slot)this.inventorySlots.get(0)).putStack(itemstack1.splitStack(1));
+                }
+                else if (!itemstack1.isEmpty())
+                {
+                    ((Slot)this.inventorySlots.get(0)).putStack(new ItemStack(itemstack1.getItem(), 1, itemstack1.getMetadata()));
+                    itemstack1.shrink(1);
+                }
+            }
+
+            if (itemstack1.isEmpty())
+            {
+                slot.putStack(ItemStack.EMPTY);
+            }
+            else
+            {
+                slot.onSlotChanged();
+            }
+
+            if (itemstack1.getCount() == itemstack.getCount())
+            {
+                return ItemStack.EMPTY;
+            }
+
+            slot.onTake(playerIn, itemstack1);
+        }
+
+        return itemstack;
     }
 }
