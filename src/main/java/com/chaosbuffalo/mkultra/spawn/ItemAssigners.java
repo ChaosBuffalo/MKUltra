8bcaaaa816949e5d924c05c31499b344815a080d
Kovak <kovac1066@gmail.com>
2019-02-17 00:54:09 -0800

    set drop chances for equipment

--- src/main/java/com/chaosbuffalo/mkultra/spawn/ItemAssigners.java
+++ src/main/java/com/chaosbuffalo/mkultra/spawn/ItemAssigners.java
@@ -1,5 +1,6 @@
 package com.chaosbuffalo.mkultra.spawn;
 
+import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.util.EnumHand;
@@ -9,25 +10,49 @@ public class ItemAssigners {
 
     public static BiConsumer<EntityLivingBase, ItemChoice> MAINHAND = (entity, choice) -> {
         entity.setHeldItem(EnumHand.MAIN_HAND, choice.item);
+        if (entity instanceof EntityLiving) {
+            EntityLiving entLiv = (EntityLiving)entity;
+            entLiv.setDropChance(EntityEquipmentSlot.MAINHAND, choice.dropChance);
+        }
     };
 
     public static BiConsumer<EntityLivingBase, ItemChoice> OFFHAND = (entity, choice) -> {
         entity.setHeldItem(EnumHand.OFF_HAND, choice.item);
+        if (entity instanceof EntityLiving) {
+            EntityLiving entLiv = (EntityLiving)entity;
+            entLiv.setDropChance(EntityEquipmentSlot.OFFHAND, choice.dropChance);
+        }
     };
 
     public static BiConsumer<EntityLivingBase, ItemChoice> HEAD = (entity, choice) -> {
         entity.setItemStackToSlot(EntityEquipmentSlot.HEAD, choice.item);
+        if (entity instanceof EntityLiving) {
+            EntityLiving entLiv = (EntityLiving)entity;
+            entLiv.setDropChance(EntityEquipmentSlot.HEAD, choice.dropChance);
+        }
     };
 
     public static BiConsumer<EntityLivingBase, ItemChoice> CHEST = (entity, choice) -> {
         entity.setItemStackToSlot(EntityEquipmentSlot.CHEST, choice.item);
+        if (entity instanceof EntityLiving) {
+            EntityLiving entLiv = (EntityLiving)entity;
+            entLiv.setDropChance(EntityEquipmentSlot.CHEST, choice.dropChance);
+        }
     };
 
     public static BiConsumer<EntityLivingBase, ItemChoice> FEET = (entity, choice) -> {
         entity.setItemStackToSlot(EntityEquipmentSlot.FEET, choice.item);
+        if (entity instanceof EntityLiving) {
+            EntityLiving entLiv = (EntityLiving)entity;
+            entLiv.setDropChance(EntityEquipmentSlot.FEET, choice.dropChance);
+        }
     };
 
     public static BiConsumer<EntityLivingBase, ItemChoice> LEGS = (entity, choice) -> {
         entity.setItemStackToSlot(EntityEquipmentSlot.LEGS, choice.item);
+        if (entity instanceof EntityLiving) {
+            EntityLiving entLiv = (EntityLiving)entity;
+            entLiv.setDropChance(EntityEquipmentSlot.LEGS, choice.dropChance);
+        }
     };
 }
--- src/main/java/com/chaosbuffalo/mkultra/spawn/ItemChoice.java
+++ src/main/java/com/chaosbuffalo/mkultra/spawn/ItemChoice.java
@@ -6,10 +6,15 @@ public class ItemChoice {
     public final ItemStack item;
     public final int weight;
     public final int minLevel;
+    public final float dropChance;
 
-    public ItemChoice(ItemStack item, int weight, int minLevel){
+    public ItemChoice(ItemStack item, int weight, int minLevel, float dropChance){
         this.item = item;
         this.weight = weight;
         this.minLevel = minLevel;
+        this.dropChance = dropChance;
+    }
+    public ItemChoice(ItemStack item, int weight, int minLevel){
+        this(item, weight, minLevel, .05f);
     }
 }
