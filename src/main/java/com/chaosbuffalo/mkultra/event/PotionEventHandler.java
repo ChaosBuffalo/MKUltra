dab3838e67df503fbceeebf3cee2e424b92113d4
Kovak <kovac1066@gmail.com>
2019-08-11 10:41:58 -0700

    forgot event subscriber annotation

--- src/main/java/com/chaosbuffalo/mkultra/event/PotionEventHandler.java
+++ src/main/java/com/chaosbuffalo/mkultra/event/PotionEventHandler.java
@@ -3,11 +3,13 @@ package com.chaosbuffalo.mkultra.event;
 import com.chaosbuffalo.mkultra.effects.SpellPotionBase;
 import net.minecraftforge.event.entity.living.PotionEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @SuppressWarnings("unused")
 @Mod.EventBusSubscriber
 public class PotionEventHandler {
 
+    @SubscribeEvent
     public static void onPotionRemove(PotionEvent.PotionRemoveEvent event){
         if (event.getPotionEffect() != null && event.getPotion() instanceof SpellPotionBase){
             SpellPotionBase spellPotion = (SpellPotionBase) event.getPotion();
