bdeeb2f967a86c925f56221c2075688e3caed1e4
Kovak <kovac1066@gmail.com>
2019-09-02 19:30:36 -0700

    mana smoke will add mana to player

--- src/main/java/com/chaosbuffalo/mkultra/effects/spells/ManaSmokeEffect.java
+++ src/main/java/com/chaosbuffalo/mkultra/effects/spells/ManaSmokeEffect.java
@@ -1,10 +1,15 @@
 package com.chaosbuffalo.mkultra.effects.spells;
 
 import com.chaosbuffalo.mkultra.MKUltra;
+import com.chaosbuffalo.mkultra.core.IPlayerData;
+import com.chaosbuffalo.mkultra.core.MKUPlayerData;
 import com.chaosbuffalo.mkultra.core.PlayerAttributes;
 import com.chaosbuffalo.mkultra.effects.PassiveEffect;
 import com.chaosbuffalo.mkultra.effects.SpellCast;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.ai.attributes.AbstractAttributeMap;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.potion.Potion;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.RegistryEvent;
@@ -19,10 +24,12 @@ import java.util.UUID;
 
 @Mod.EventBusSubscriber(modid = MKUltra.MODID)
 public class ManaSmokeEffect extends PassiveEffect {
+
+    private static final double AMOUNT = 10.0;
     public static final UUID MODIFIER_ID = UUID.fromString("faa79ab5-d8c6-4737-8a28-01a00af63cf0");
     public static final ManaSmokeEffect INSTANCE = (ManaSmokeEffect) (new ManaSmokeEffect()
             .registerPotionAttributeModifier(PlayerAttributes.MAX_MANA,
-                    MODIFIER_ID.toString(), 10.0, PlayerAttributes.OP_INCREMENT)
+                    MODIFIER_ID.toString(), AMOUNT, PlayerAttributes.OP_INCREMENT)
     );
 
     @SubscribeEvent
@@ -37,6 +44,19 @@ public class ManaSmokeEffect extends PassiveEffect {
     private ManaSmokeEffect() {
         super(false, 1665535);
         setPotionName("effect.mana_smoke");
+    }
+
+
+    @Override
+    public void onPotionAdd(SpellCast cast, EntityLivingBase target, AbstractAttributeMap attributes, int amplifier) {
+        super.onPotionAdd(cast, target, attributes, amplifier);
+        if (target instanceof EntityPlayer){
+            IPlayerData data = MKUPlayerData.get((EntityPlayer) target);
+            if (data != null){
+                data.addMana((float) (AMOUNT * amplifier));
+            }
+        }
+
     }
 
     @Override
