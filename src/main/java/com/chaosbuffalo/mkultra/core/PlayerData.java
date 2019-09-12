f23a6bc44a0d472b9f9c8f0f9a71fef9fe04e001
Lee Harrison <ralekdev@gmail.com>
2019-09-11 22:57:51 -0700

    Remove DiamondDust special case when learning classes

--- src/main/java/com/chaosbuffalo/mkultra/core/PlayerData.java
+++ src/main/java/com/chaosbuffalo/mkultra/core/PlayerData.java
@@ -1284,10 +1284,6 @@ public class PlayerData implements IPlayerData {
 
     @Override
     public boolean learnClass(IClassProvider provider, ResourceLocation classId) {
-        return learnClass(provider, classId, true);
-    }
-
-    public boolean learnClass(IClassProvider provider, ResourceLocation classId, boolean enforceChecks) {
         if (isClassKnown(classId)) {
             // Class was already known
             return true;
@@ -1297,7 +1293,7 @@ public class PlayerData implements IPlayerData {
         if (playerClass == null)
             return false;
 
-        if (enforceChecks && !provider.teachesClass(playerClass))
+        if (!provider.teachesClass(playerClass))
             return false;
 
         PlayerClassInfo info = new PlayerClassInfo(classId);
