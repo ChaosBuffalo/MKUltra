c625a74ab5e83f219acb3a1cbee4d2ae88b34517
Lee Harrison <ralekdev@gmail.com>
2019-11-01 21:22:20 -0700

    Allow PlayerAbility implementations to draw custom slot effects

--- src/main/java/com/chaosbuffalo/mkultra/core/PlayerToggleAbility.java
+++ src/main/java/com/chaosbuffalo/mkultra/core/PlayerToggleAbility.java
@@ -1,10 +1,16 @@
 package com.chaosbuffalo.mkultra.core;
 
+import com.chaosbuffalo.mkultra.MKUltra;
+import com.chaosbuffalo.mkultra.client.gui.AbilityBar;
 import com.google.common.collect.Maps;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.potion.Potion;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 import java.util.Map;
 
@@ -14,6 +20,8 @@ import java.util.Map;
 public abstract class PlayerToggleAbility extends PlayerAbility {
 
     private static final Map<Potion, ResourceLocation> toggleAbilityMap = Maps.newHashMap();
+    private static final ResourceLocation TOGGLE_EFFECT = new ResourceLocation(MKUltra.MODID,
+            "textures/class/abilities/ability_toggle.png");
 
     public static ResourceLocation getToggleAbilityIdForPotion(Potion potion) {
         return toggleAbilityMap.get(potion);
@@ -58,4 +66,13 @@ public abstract class PlayerToggleAbility extends PlayerAbility {
         }
     }
 
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void drawAbilityBarEffect(Minecraft mc, int slotX, int slotY) {
+        if (mc.player.isPotionActive(getToggleEffect())) {
+            int iconSize = AbilityBar.ABILITY_ICON_SIZE;
+            mc.getTextureManager().bindTexture(TOGGLE_EFFECT);
+            Gui.drawModalRectWithCustomSizedTexture(slotX, slotY, 0, 0, iconSize, iconSize, iconSize, iconSize);
+        }
+    }
 }
