a0f70e103b3c1003c000e8e26819138531b1cfff
Lee Harrison <ralekdev@gmail.com>
2022-01-07 13:32:37 -0800

    Port Power Word Summon

--- src/main/java/com/chaosbuffalo/mkultra/effects/spells/WarpTargetEffect.java
+++ src/main/java/com/chaosbuffalo/mkultra/effects/spells/WarpTargetEffect.java
@@ -1,49 +1,61 @@
 package com.chaosbuffalo.mkultra.effects.spells;
 
-import com.chaosbuffalo.mkcore.effects.SpellCast;
-import com.chaosbuffalo.mkcore.effects.SpellEffectBase;
+import com.chaosbuffalo.mkcore.core.IMKEntityData;
+import com.chaosbuffalo.mkcore.effects.MKActiveEffect;
+import com.chaosbuffalo.mkcore.effects.MKEffect;
+import com.chaosbuffalo.mkcore.effects.MKEffectBuilder;
+import com.chaosbuffalo.mkcore.effects.MKEffectState;
 import com.chaosbuffalo.mkultra.MKUltra;
-import com.chaosbuffalo.targeting_api.TargetingContext;
-import com.chaosbuffalo.targeting_api.TargetingContexts;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.potion.Effect;
 import net.minecraft.potion.EffectType;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
+import java.util.UUID;
+
 @Mod.EventBusSubscriber(modid = MKUltra.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
-public class WarpTargetEffect extends SpellEffectBase {
+public class WarpTargetEffect extends MKEffect {
 
     public static final WarpTargetEffect INSTANCE = new WarpTargetEffect();
 
     @SubscribeEvent
-    public static void register(RegistryEvent.Register<Effect> event) {
+    public static void register(RegistryEvent.Register<MKEffect> event) {
         event.getRegistry().register(INSTANCE);
     }
 
-    public static SpellCast Create(Entity source) {
-        return INSTANCE.newSpellCast(source);
-    }
-
     private WarpTargetEffect() {
-        // boolean isBadEffectIn, int liquidColorIn
-        super(EffectType.HARMFUL, 4393423);
+        super(EffectType.HARMFUL);
         setRegistryName(MKUltra.MODID, "effect.warp_target");
     }
 
     @Override
-    public TargetingContext getTargetContext() {
-        return TargetingContexts.ALL;
+    public State makeState() {
+        return new State();
     }
 
     @Override
-    public void doEffect(Entity applier, Entity caster, LivingEntity target, int amplifier, SpellCast cast) {
-        Vector3d playerOrigin = caster.getPositionVec();
-        Vector3d heading = caster.getLookVec();
-        target.setPositionAndUpdate(playerOrigin.x + heading.x, playerOrigin.y + heading.y + 1.0,
-                playerOrigin.z + heading.z);
+    public MKEffectBuilder<State> builder(UUID sourceId) {
+        return new MKEffectBuilder<>(this, sourceId, this::makeState);
+    }
+
+    public static class State extends MKEffectState {
+        private Entity source;
+
+        @Override
+        public boolean performEffect(IMKEntityData targetData, MKActiveEffect activeEffect) {
+            source = findEntity(source, activeEffect.getSourceId(), targetData);
+            if (source == null) {
+                return false;
+            }
+            Vector3d playerOrigin = source.getPositionVec();
+            Vector3d heading = source.getLookVec();
+            targetData.getEntity().setPositionAndUpdate(
+                    playerOrigin.x + heading.x,
+                    playerOrigin.y + heading.y + 1.0,
+                    playerOrigin.z + heading.z);
+            return true;
+        }
     }
 }
