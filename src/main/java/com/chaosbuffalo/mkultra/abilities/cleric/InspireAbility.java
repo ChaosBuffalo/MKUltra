fdcddbb2d2fa5019dc68127db20335dc2acee854
Lee Harrison <ralekdev@gmail.com>
2022-01-07 13:02:45 -0800

    Port Inspire

--- src/main/java/com/chaosbuffalo/mkultra/abilities/cleric/InspireAbility.java
+++ src/main/java/com/chaosbuffalo/mkultra/abilities/cleric/InspireAbility.java
@@ -1,25 +1,24 @@
 package com.chaosbuffalo.mkultra.abilities.cleric;
 
 import com.chaosbuffalo.mkcore.GameConstants;
-import com.chaosbuffalo.mkcore.abilities.*;
+import com.chaosbuffalo.mkcore.abilities.AbilityContext;
+import com.chaosbuffalo.mkcore.abilities.AbilityTargetSelector;
+import com.chaosbuffalo.mkcore.abilities.AbilityTargeting;
+import com.chaosbuffalo.mkcore.abilities.MKAbility;
 import com.chaosbuffalo.mkcore.core.AbilityType;
-import com.chaosbuffalo.mkcore.effects.MKParticleEffect;
-import com.chaosbuffalo.mkcore.effects.SpellCast;
-import com.chaosbuffalo.mkcore.serialization.attributes.IntAttribute;
 import com.chaosbuffalo.mkcore.core.IMKEntityData;
 import com.chaosbuffalo.mkcore.core.MKAttributes;
 import com.chaosbuffalo.mkcore.effects.AreaEffectBuilder;
-import com.chaosbuffalo.mkcore.effects.instant.SoundEffect;
-import com.chaosbuffalo.mkcore.fx.ParticleEffects;
-import com.chaosbuffalo.mkcore.network.PacketHandler;
-import com.chaosbuffalo.mkcore.network.ParticleEffectSpawnPacket;
+import com.chaosbuffalo.mkcore.effects.MKEffectBuilder;
+import com.chaosbuffalo.mkcore.effects.MKParticleEffectNew;
+import com.chaosbuffalo.mkcore.effects.instant.SoundEffectNew;
+import com.chaosbuffalo.mkcore.serialization.attributes.IntAttribute;
 import com.chaosbuffalo.mkcore.serialization.attributes.ResourceLocationAttribute;
 import com.chaosbuffalo.mkultra.MKUltra;
 import com.chaosbuffalo.mkultra.init.ModSounds;
 import com.chaosbuffalo.targeting_api.TargetingContext;
 import com.chaosbuffalo.targeting_api.TargetingContexts;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.particles.ParticleTypes;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
 import net.minecraft.util.ResourceLocation;
@@ -31,12 +30,9 @@ import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
-import java.util.Set;
-
 @Mod.EventBusSubscriber(modid = MKUltra.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class InspireAbility extends MKAbility {
-    public static final ResourceLocation CASTING_PARTICLES = new ResourceLocation(MKUltra.MODID, "inspire_casting");
-    public static final ResourceLocation CAST_PARTICLES = new ResourceLocation(MKUltra.MODID, "inspire_cast");
+
     public static final InspireAbility INSTANCE = new InspireAbility();
 
     @SubscribeEvent
@@ -44,6 +40,8 @@ public class InspireAbility extends MKAbility {
         event.getRegistry().register(INSTANCE);
     }
 
+    protected final ResourceLocation CASTING_PARTICLES = new ResourceLocation(MKUltra.MODID, "inspire_casting");
+    protected final ResourceLocation CAST_PARTICLES = new ResourceLocation(MKUltra.MODID, "inspire_cast");
     protected final IntAttribute base = new IntAttribute("baseDuration", 8);
     protected final IntAttribute scale = new IntAttribute("scaleDuration", 2);
     protected final ResourceLocationAttribute cast_particles = new ResourceLocationAttribute("cast_particles", CAST_PARTICLES);
@@ -56,13 +54,12 @@ public class InspireAbility extends MKAbility {
         addAttributes(base, scale, cast_particles);
         addSkillAttribute(MKAttributes.ALTERATON);
         casting_particles.setDefaultValue(CASTING_PARTICLES);
-
     }
 
     @Override
     protected ITextComponent getAbilityDescription(IMKEntityData entityData) {
         int level = getSkillLevel(entityData.getEntity(), MKAttributes.ALTERATON);
-        int duration = getBuffDuration(entityData, level, base.getValue(), scale.getValue()) / GameConstants.TICKS_PER_SECOND;
+        int duration = getBuffDuration(entityData, level, base.value(), scale.value()) / GameConstants.TICKS_PER_SECOND;
         return new TranslationTextComponent(getDescriptionTranslationKey(), duration);
     }
 
@@ -98,23 +95,29 @@ public class InspireAbility extends MKAbility {
     }
 
     @Override
-    public void endCast(LivingEntity entity, IMKEntityData data, AbilityContext context) {
-        super.endCast(entity, data, context);
-        int level = getSkillLevel(entity, MKAttributes.ALTERATON);
-        int duration = getBuffDuration(data, level, base.getValue(), scale.getValue());
+    public void endCast(LivingEntity castingEntity, IMKEntityData casterData, AbilityContext context) {
+        super.endCast(castingEntity, casterData, context);
+        int level = getSkillLevel(castingEntity, MKAttributes.ALTERATON);
+        int duration = getBuffDuration(casterData, level, base.value(), scale.value());
 
         EffectInstance hasteEffect = new EffectInstance(Effects.HASTE, duration, level, false, false);
         EffectInstance regenEffect = new EffectInstance(Effects.REGENERATION, duration, level, false, false);
-        SpellCast particles = MKParticleEffect.Create(entity, cast_particles.getValue(),
-                true, new Vector3d(0.0, 1.0, 0.0));
+        MKEffectBuilder<?> sound = SoundEffectNew.INSTANCE.builder(castingEntity.getUniqueID())
+                .ability(this)
+                .state(s -> s.setup(ModSounds.spell_holy_8, castingEntity.getSoundCategory()));
+        MKEffectBuilder<?> particles = MKParticleEffectNew.INSTANCE.builder(castingEntity.getUniqueID())
+                .ability(this)
+                .state(s -> s.setup(cast_particles.getValue(), true, new Vector3d(0.0, 1.0, 0.0)))
+                .amplify(level);
 
-        AreaEffectBuilder.Create(entity, entity)
+        AreaEffectBuilder.createOnCaster(castingEntity)
                 .effect(hasteEffect, getTargetContext())
                 .effect(regenEffect, getTargetContext())
-                .spellCast(SoundEffect.Create(entity, ModSounds.spell_holy_8, entity.getSoundCategory()),
-                        1, getTargetContext())
-                .spellCast(particles, level, getTargetContext())
-                .instant().color(1034415).radius(getDistance(entity), true)
+                .effect(sound, getTargetContext())
+                .effect(particles, getTargetContext())
+                .instant()
+                .color(1034415)
+                .radius(getDistance(castingEntity), true)
                 .disableParticle()
                 .spawn();
     }
