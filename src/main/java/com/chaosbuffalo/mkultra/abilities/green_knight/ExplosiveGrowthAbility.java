6bece5235959c264f0ed298bf975b8d41779080a
Lee Harrison <ralekdev@gmail.com>
2022-01-07 15:15:30 -0800

    Port Explosive Growth

--- src/main/java/com/chaosbuffalo/mkultra/abilities/green_knight/ExplosiveGrowthAbility.java
+++ src/main/java/com/chaosbuffalo/mkultra/abilities/green_knight/ExplosiveGrowthAbility.java
@@ -1,29 +1,30 @@
 package com.chaosbuffalo.mkultra.abilities.green_knight;
 
 import com.chaosbuffalo.mkcore.GameConstants;
-import com.chaosbuffalo.mkcore.abilities.*;
+import com.chaosbuffalo.mkcore.MKCore;
+import com.chaosbuffalo.mkcore.abilities.AbilityContext;
+import com.chaosbuffalo.mkcore.abilities.MKAbility;
 import com.chaosbuffalo.mkcore.core.AbilityType;
-import com.chaosbuffalo.mkcore.network.MKParticleEffectSpawnPacket;
-import com.chaosbuffalo.mkcore.serialization.attributes.FloatAttribute;
 import com.chaosbuffalo.mkcore.core.IMKEntityData;
 import com.chaosbuffalo.mkcore.core.MKAttributes;
 import com.chaosbuffalo.mkcore.core.damage.MKDamageSource;
-import com.chaosbuffalo.mkcore.fx.ParticleEffects;
+import com.chaosbuffalo.mkcore.effects.MKEffectBuilder;
 import com.chaosbuffalo.mkcore.init.CoreDamageTypes;
+import com.chaosbuffalo.mkcore.network.MKParticleEffectSpawnPacket;
 import com.chaosbuffalo.mkcore.network.PacketHandler;
-import com.chaosbuffalo.mkcore.network.ParticleEffectSpawnPacket;
+import com.chaosbuffalo.mkcore.serialization.attributes.FloatAttribute;
 import com.chaosbuffalo.mkcore.serialization.attributes.ResourceLocationAttribute;
 import com.chaosbuffalo.mkcore.utils.RayTraceUtils;
 import com.chaosbuffalo.mkcore.utils.SoundUtils;
+import com.chaosbuffalo.mkcore.utils.TargetUtil;
 import com.chaosbuffalo.mkultra.MKUltra;
-import com.chaosbuffalo.mkultra.effects.spells.CureEffect;
+import com.chaosbuffalo.mkultra.effects.spells.CureEffectV2;
 import com.chaosbuffalo.mkultra.init.ModSounds;
 import com.chaosbuffalo.targeting_api.Targeting;
 import com.chaosbuffalo.targeting_api.TargetingContext;
 import com.chaosbuffalo.targeting_api.TargetingContexts;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.particles.ParticleTypes;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvent;
@@ -37,7 +38,6 @@ import net.minecraftforge.fml.common.Mod;
 
 import javax.annotation.Nullable;
 import java.util.List;
-import java.util.Set;
 
 @Mod.EventBusSubscriber(modid = MKUltra.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ExplosiveGrowthAbility extends MKAbility {
@@ -56,7 +56,6 @@ public class ExplosiveGrowthAbility extends MKAbility {
     protected final FloatAttribute modifierScaling = new FloatAttribute("modifierScaling", 1.0f);
     protected final ResourceLocationAttribute cast_particles = new ResourceLocationAttribute("cast_particles", CAST_PARTICLES);
     protected final ResourceLocationAttribute detonate_particles = new ResourceLocationAttribute("detonate_particles", DETONATE_PARTICLES);
-
 
     private ExplosiveGrowthAbility() {
         super(MKUltra.MODID, "ability.explosive_growth");
@@ -81,9 +80,10 @@ public class ExplosiveGrowthAbility extends MKAbility {
 
     @Override
     protected ITextComponent getAbilityDescription(IMKEntityData entityData) {
-        ITextComponent damageStr = getDamageDescription(entityData, CoreDamageTypes.MeleeDamage, baseDamage.getValue(),
-                scaleDamage.getValue(), getSkillLevel(entityData.getEntity(), MKAttributes.PANKRATION),
-                modifierScaling.getValue());
+        ITextComponent damageStr = getDamageDescription(entityData, CoreDamageTypes.MeleeDamage, baseDamage.value(),
+                scaleDamage.value(),
+                getSkillLevel(entityData.getEntity(), MKAttributes.PANKRATION),
+                modifierScaling.value());
         return new TranslationTextComponent(getDescriptionTranslationKey(), damageStr);
     }
 
@@ -104,49 +104,55 @@ public class ExplosiveGrowthAbility extends MKAbility {
     }
 
     @Override
-    public void endCast(LivingEntity entity, IMKEntityData data, AbilityContext context) {
-        super.endCast(entity, data, context);
-        int restoLevel = getSkillLevel(entity, MKAttributes.RESTORATION);
-        int pankrationLevel = getSkillLevel(entity, MKAttributes.PANKRATION);
-        Vector3d look = entity.getLookVec().scale(getDistance(entity));
-        Vector3d from = entity.getPositionVec().add(0, entity.getEyeHeight(), 0);
+    public void endCast(LivingEntity castingEntity, IMKEntityData casterData, AbilityContext context) {
+        super.endCast(castingEntity, casterData, context);
+        int restoLevel = getSkillLevel(castingEntity, MKAttributes.RESTORATION);
+        int pankrationLevel = getSkillLevel(castingEntity, MKAttributes.PANKRATION);
+
+        SoundCategory cat = castingEntity instanceof PlayerEntity ? SoundCategory.PLAYERS : SoundCategory.HOSTILE;
+        float damage = baseDamage.value() + scaleDamage.value() * pankrationLevel;
+
+        MKEffectBuilder<?> cure = CureEffectV2.INSTANCE.builder(castingEntity.getUniqueID())
+                .ability(this)
+                .amplify(restoLevel);
+
+        Vector3d look = castingEntity.getLookVec().scale(getDistance(castingEntity));
+        Vector3d from = castingEntity.getPositionVec().add(0, castingEntity.getEyeHeight(), 0);
         Vector3d to = from.add(look);
-        List<LivingEntity> entityHit = getTargetsInLine(entity, from, to, true, 1.0f);
-        SoundCategory cat = entity instanceof PlayerEntity ? SoundCategory.PLAYERS : SoundCategory.HOSTILE;
-        float damage = baseDamage.getValue() + scaleDamage.getValue() * pankrationLevel;
+        List<LivingEntity> entityHit = TargetUtil.getTargetsInLine(castingEntity, from, to, 1.0f, this::isValidTarget);
+
         for (LivingEntity entHit : entityHit) {
-            if (Targeting.isValidEnemy(entity, entHit)) {
-                entHit.attackEntityFrom(MKDamageSource.causeMeleeDamage(getAbilityId(), entity, entity), damage);
-                SoundUtils.serverPlaySoundAtEntity(entHit, ModSounds.spell_earth_1, cat);
-            } else if (!entHit.equals(entity) && Targeting.isValidFriendly(entity, entHit)) {
-                entHit.addPotionEffect(CureEffect.Create(entity).setTarget(entHit).toPotionEffect(restoLevel));
-                NaturesRemedyAbility.INSTANCE.castNaturesRemedyOnTarget(entHit, data, restoLevel);
-                SoundUtils.serverPlaySoundAtEntity(entHit, ModSounds.spell_earth_6, cat);
+            Targeting.TargetRelation relation = Targeting.getTargetRelation(castingEntity, entHit);
+            switch (relation) {
+                case FRIEND: {
+                    MKCore.getEntityData(entHit).ifPresent(targetData -> targetData.getEffects().addEffect(cure));
+
+                    NaturesRemedyAbility.INSTANCE.castNaturesRemedyOnTarget(entHit, casterData, restoLevel);
+                    SoundUtils.serverPlaySoundAtEntity(entHit, ModSounds.spell_earth_6, cat);
+                    break;
+                }
+                case ENEMY: {
+                    entHit.attackEntityFrom(MKDamageSource.causeMeleeDamage(getAbilityId(), castingEntity, castingEntity), damage);
+                    SoundUtils.serverPlaySoundAtEntity(entHit, ModSounds.spell_earth_1, cat);
+                    break;
+                }
             }
+
             PacketHandler.sendToTrackingAndSelf(new MKParticleEffectSpawnPacket(
-                        new Vector3d(0.0, 1.0, 0.0), detonate_particles.getValue(),
-                        entHit.getEntityId()),
-                entHit);
+                    new Vector3d(0.0, 1.0, 0.0), detonate_particles.getValue(),
+                    entHit.getEntityId()), entHit);
         }
-        RayTraceResult blockHit = RayTraceUtils.rayTraceBlocks(entity, from, to, false);
+
+        RayTraceResult blockHit = RayTraceUtils.rayTraceBlocks(castingEntity, from, to, false);
         if (blockHit != null && blockHit.getType() == RayTraceResult.Type.BLOCK) {
             to = blockHit.getHitVec();
         }
-        entity.addPotionEffect(CureEffect.Create(entity).setTarget(entity).toPotionEffect(restoLevel));
-        NaturesRemedyAbility.INSTANCE.castNaturesRemedyOnTarget(entity, data, restoLevel);
-        entity.setPositionAndUpdate(to.x, to.y, to.z);
+
+        casterData.getEffects().addEffect(cure);
+        NaturesRemedyAbility.INSTANCE.castNaturesRemedyOnTarget(castingEntity, casterData, restoLevel);
+        castingEntity.setPositionAndUpdate(to.x, to.y, to.z);
         MKParticleEffectSpawnPacket spawn = new MKParticleEffectSpawnPacket(from, CAST_PARTICLES);
         spawn.addLoc(to);
-        PacketHandler.sendToTrackingAndSelf(spawn, entity);
-
-//        PacketHandler.sendToTrackingAndSelf(
-//                new ParticleEffectSpawnPacket(
-//                        ParticleTypes.AMBIENT_ENTITY_EFFECT,
-//                        ParticleEffects.SPHERE_MOTION, 30, 10,
-//                        entity.getPosX(), entity.getPosY() + 1.0,
-//                        entity.getPosZ(), 1.0, 1.0, 1.0, 2.0,
-//                        lookVec),
-//                entity);
+        PacketHandler.sendToTrackingAndSelf(spawn, castingEntity);
     }
-
 }
