1ee72d2b5aa2584406dc378e637ec7fb596ad4c8
Kovak <kovac1066@gmail.com>
2022-01-17 20:39:09 -0800

    some more piglin variant textures and yank, whirlwind, and furious brooding impl

--- src/main/java/com/chaosbuffalo/mkultra/abilities/brawler/FuriousBrooding.java
+++ src/main/java/com/chaosbuffalo/mkultra/abilities/brawler/FuriousBrooding.java
@@ -1,98 +1,109 @@
-//package com.chaosbuffalo.mkultra.abilities.brawler;
-//
-//import com.chaosbuffalo.mkultra.GameConstants;
-//import com.chaosbuffalo.mkultra.MKUltra;
-//import com.chaosbuffalo.mkultra.core.IPlayerData;
-//import com.chaosbuffalo.mkultra.core.PlayerAbility;
-//import com.chaosbuffalo.mkultra.core.PlayerFormulas;
-//import com.chaosbuffalo.mkultra.fx.ParticleEffects;
-//import com.chaosbuffalo.mkultra.init.ModSounds;
-//import com.chaosbuffalo.mkultra.network.packets.ParticleEffectSpawnPacket;
-//import com.chaosbuffalo.targeting_api.Targeting;
-//import net.minecraft.entity.player.EntityPlayer;
-//import net.minecraft.init.MobEffects;
-//import net.minecraft.potion.PotionEffect;
-//import net.minecraft.util.EnumParticleTypes;
-//import net.minecraft.util.SoundEvent;
-//import net.minecraft.util.math.Vec3d;
-//import net.minecraft.world.World;
-//import net.minecraftforge.event.RegistryEvent;
-//import net.minecraftforge.fml.common.Mod;
-//import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-//
-//import javax.annotation.Nullable;
-//
-//@Mod.EventBusSubscriber(modid = MKUltra.MODID)
-//public class FuriousBrooding extends PlayerAbility {
-//    public static FuriousBrooding INSTANCE = new FuriousBrooding();
-//
-//    @SubscribeEvent
-//    public static void register(RegistryEvent.Register<PlayerAbility> event) {
-//        event.getRegistry().register(INSTANCE.setRegistryName(INSTANCE.getAbilityId()));
-//    }
-//
-//    private FuriousBrooding() {
-//        super(MKUltra.MODID, "ability.furious_brooding");
-//    }
-//
-//    @Override
-//    public int getCooldown(int currentRank) {
-//        return 18;
-//    }
-//
-//    @Override
-//    public Targeting.TargetType getTargetType() {
-//        return Targeting.TargetType.SELF;
-//    }
-//
-//    @Override
-//    public float getManaCost(int currentRank) {
-//        return 4 + 2 * currentRank;
-//    }
-//
-//    @Override
-//    public float getDistance(int currentRank) {
-//        return 1.0f;
-//    }
-//
-//    @Nullable
-//    @Override
-//    public SoundEvent getSpellCompleteSoundEvent() {
-//        return ModSounds.spell_negative_effect_7;
-//    }
-//
-//    @Override
-//    public int getRequiredLevel(int currentRank) {
-//        return currentRank * 2;
-//    }
-//
-//    @Override
-//    public void execute(EntityPlayer entity, IPlayerData pData, World theWorld) {
-//
-//        int level = pData.getAbilityRank(getAbilityId());
-//
-//        pData.startAbility(this);
-//
-//        int duration = (6 + 5 * level) * GameConstants.TICKS_PER_SECOND;
-//        duration = PlayerFormulas.applyBuffDurationBonus(pData, duration);
-//
-//        entity.addPotionEffect(
-//                new PotionEffect(MobEffects.REGENERATION, duration, level + 1, false, true));
-//        entity.addPotionEffect(
-//                new PotionEffect(MobEffects.SLOWNESS, duration, level - 1, false, true));
-//
-//        Vec3d lookVec = entity.getLookVec();
-//        MKUltra.packetHandler.sendToAllAround(
-//                new ParticleEffectSpawnPacket(
-//                        EnumParticleTypes.VILLAGER_ANGRY.getParticleID(),
-//                        ParticleEffects.DIRECTED_SPOUT, 60, 1,
-//                        entity.posX, entity.posY + 1.0,
-//                        entity.posZ, 1.0, 1.0, 1.0, 1.0,
-//                        lookVec),
-//                entity.dimension, entity.posX,
-//                entity.posY, entity.posZ, 50.0f);
-//    }
-//}
-//
-//
-//
+package com.chaosbuffalo.mkultra.abilities.brawler;
+
+import com.chaosbuffalo.mkcore.GameConstants;
+import com.chaosbuffalo.mkcore.MKCore;
+import com.chaosbuffalo.mkcore.abilities.*;
+import com.chaosbuffalo.mkcore.abilities.ai.conditions.HealCondition;
+import com.chaosbuffalo.mkcore.core.IMKEntityData;
+import com.chaosbuffalo.mkcore.core.MKAttributes;
+import com.chaosbuffalo.mkcore.effects.MKEffectBuilder;
+import com.chaosbuffalo.mkcore.network.MKParticleEffectSpawnPacket;
+import com.chaosbuffalo.mkcore.network.PacketHandler;
+import com.chaosbuffalo.mkcore.serialization.attributes.FloatAttribute;
+import com.chaosbuffalo.mkcore.serialization.attributes.IntAttribute;
+import com.chaosbuffalo.mkcore.serialization.attributes.ResourceLocationAttribute;
+import com.chaosbuffalo.mkultra.MKUltra;
+import com.chaosbuffalo.mkultra.effects.FuriousBroodingEffect;
+import com.chaosbuffalo.mkultra.init.ModSounds;
+import com.chaosbuffalo.targeting_api.TargetingContext;
+import com.chaosbuffalo.targeting_api.TargetingContexts;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import javax.annotation.Nullable;
+
+public class FuriousBrooding extends MKAbility {
+    public static final ResourceLocation TICK_PARTICLES = new ResourceLocation(MKUltra.MODID, "furious_brooding_pulse");
+    public static final FuriousBrooding INSTANCE = new FuriousBrooding();
+
+    protected final ResourceLocationAttribute tick_particles = new ResourceLocationAttribute("cast_particles", TICK_PARTICLES);
+    protected final FloatAttribute baseValue = new FloatAttribute("baseValue", 2.0f);
+    protected final FloatAttribute scaleValue = new FloatAttribute("scaleValue", 1.0f);
+    protected final IntAttribute baseDuration = new IntAttribute("baseDuration", 6);
+    protected final IntAttribute scaleDuration = new IntAttribute("scaleDuration", 5);
+    protected final FloatAttribute modifierScaling = new FloatAttribute("modifierScaling", 1.0f);
+
+    private FuriousBrooding() {
+        super(new ResourceLocation(MKUltra.MODID, "ability.furious_brooding"));
+        setCooldownSeconds(18);
+        setManaCost(6);
+        addAttributes(tick_particles, baseValue, baseDuration, scaleValue, scaleDuration, modifierScaling);
+        addSkillAttribute(MKAttributes.PNEUMA);
+        setUseCondition(new HealCondition(this, 0.8f).setSelfOnly(true));
+    }
+
+    @Override
+    public TargetingContext getTargetContext() {
+        return TargetingContexts.SELF;
+    }
+
+    @Override
+    public AbilityTargetSelector getTargetSelector() {
+        return AbilityTargeting.SELF;
+    }
+
+    @Override
+    protected ITextComponent getSkillDescription(IMKEntityData casterData) {
+        float level = getSkillLevel(casterData.getEntity(), MKAttributes.PNEUMA);
+        ITextComponent damageStr = getHealDescription(casterData, baseValue.value(),
+                scaleValue.value(), level, modifierScaling.value());
+        int duration = getBuffDuration(casterData, level, baseDuration.value(), scaleDuration.value()) / GameConstants.TICKS_PER_SECOND;
+        float speedReduction = (10.0f - level) * 0.06f + 0.06f;
+        return new TranslationTextComponent(getDescriptionTranslationKey(), damageStr, INTEGER_FORMATTER.format(duration), PERCENT_FORMATTER.format(speedReduction));
+    }
+
+    @Nullable
+    @Override
+    public SoundEvent getSpellCompleteSoundEvent() {
+        return ModSounds.spell_negative_effect_7;
+    }
+
+    public MKEffectBuilder<?> createFuriousBroodingEffect(IMKEntityData casterData, float level) {
+        int duration = getBuffDuration(casterData, level, baseDuration.value(), scaleDuration.value());
+        return FuriousBroodingEffect.from(casterData.getEntity(), baseValue.value(), scaleValue.value(),
+                modifierScaling.value(), tick_particles.getValue(), level)
+                .ability(this)
+                .skillLevel(10.0f - level)
+                .timed(duration);
+    }
+
+    @Override
+    public void endCast(LivingEntity castingEntity, IMKEntityData casterData, AbilityContext context) {
+        super.endCast(castingEntity, casterData, context);
+        float level = getSkillLevel(castingEntity, MKAttributes.PNEUMA);
+        context.getMemory(MKAbilityMemories.ABILITY_TARGET).ifPresent(targetEntity -> {
+            MKEffectBuilder<?> heal = createFuriousBroodingEffect(casterData, level).ability(this);
+            MKCore.getEntityData(targetEntity).ifPresent(targetData -> targetData.getEffects().addEffect(heal));
+            PacketHandler.sendToTrackingAndSelf(new MKParticleEffectSpawnPacket(
+                    new Vector3d(0.0, 1.0, 0.0), tick_particles.getValue(),
+                    targetEntity.getEntityId()), targetEntity);
+        });
+    }
+
+    @SuppressWarnings("unused")
+    @Mod.EventBusSubscriber(modid = MKUltra.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    private static class RegisterMe {
+        @SubscribeEvent
+        public static void register(RegistryEvent.Register<MKAbility> event) {
+            event.getRegistry().register(INSTANCE);
+        }
+    }
+}
+
--- src/main/java/com/chaosbuffalo/mkultra/abilities/brawler/WhirlwindBlades.java
+++ src/main/java/com/chaosbuffalo/mkultra/abilities/brawler/WhirlwindBlades.java
@@ -1,132 +1,157 @@
-//package com.chaosbuffalo.mkultra.abilities.brawler;
-//
-//import com.chaosbuffalo.mkultra.GameConstants;
-//import com.chaosbuffalo.mkultra.MKUltra;
-//import com.chaosbuffalo.mkultra.abilities.cast_states.CastState;
-//import com.chaosbuffalo.mkultra.core.IPlayerData;
-//import com.chaosbuffalo.mkultra.core.PlayerAbility;
-//import com.chaosbuffalo.mkultra.effects.AreaEffectBuilder;
-//import com.chaosbuffalo.mkultra.effects.SpellCast;
-//import com.chaosbuffalo.mkultra.effects.spells.AbilityMeleeDamage;
-//import com.chaosbuffalo.mkultra.effects.spells.ParticlePotion;
-//import com.chaosbuffalo.mkultra.effects.spells.SoundPotion;
-//import com.chaosbuffalo.mkultra.fx.ParticleEffects;
-//import com.chaosbuffalo.mkultra.init.ModSounds;
-//import com.chaosbuffalo.mkultra.network.packets.ParticleEffectSpawnPacket;
-//import com.chaosbuffalo.targeting_api.Targeting;
-//import net.minecraft.entity.player.EntityPlayer;
-//import net.minecraft.util.EnumParticleTypes;
-//import net.minecraft.util.SoundCategory;
-//import net.minecraft.util.SoundEvent;
-//import net.minecraft.util.math.Vec3d;
-//import net.minecraft.world.World;
-//import net.minecraftforge.event.RegistryEvent;
-//import net.minecraftforge.fml.common.Mod;
-//import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-//
-//import javax.annotation.Nullable;
-//
-//@Mod.EventBusSubscriber(modid = MKUltra.MODID)
-//public class WhirlwindBlades extends PlayerAbility {
-//    public static WhirlwindBlades INSTANCE = new WhirlwindBlades();
-//
-//    @SubscribeEvent
-//    public static void register(RegistryEvent.Register<PlayerAbility> event) {
-//        event.getRegistry().register(INSTANCE.setRegistryName(INSTANCE.getAbilityId()));
-//    }
-//
-//    public static float BASE_DAMAGE = 2.0f;
-//    public static float DAMAGE_SCALE = 1.0f;
-//
-//    private WhirlwindBlades() {
-//        super(MKUltra.MODID, "ability.whirlwind_blades");
-//    }
-//
-//    @Override
-//    public int getCooldown(int currentRank) {
-//        return 25 - 5 * currentRank;
-//    }
-//
-//    @Override
-//    public Targeting.TargetType getTargetType() {
-//        return Targeting.TargetType.ENEMY;
-//    }
-//
-//    @Override
-//    public float getManaCost(int currentRank) {
-//        return 4 + 2 * currentRank;
-//    }
-//
-//    @Override
-//    public float getDistance(int currentRank) {
-//        return 3.0f + currentRank * 1.0f;
-//    }
-//
-//    @Override
-//    public int getRequiredLevel(int currentRank) {
-//        return 6 + currentRank * 2;
-//    }
-//
-//    @Override
-//    public SoundEvent getCastingSoundEvent() {
-//        return ModSounds.spell_whirlwind_1;
-//    }
-//
-//    @Nullable
-//    @Override
-//    public SoundEvent getSpellCompleteSoundEvent() {
-//        return null;
-//    }
-//
-//    @Override
-//    public int getCastTime(int currentRank) {
-//        return currentRank * GameConstants.TICKS_PER_SECOND * 3;
-//    }
-//
-//    @Override
-//    public void continueCast(EntityPlayer entity, IPlayerData data, World theWorld, int castTimeLeft, CastState state) {
-//        super.continueCast(entity, data, theWorld, castTimeLeft, state);
-//        int tickSpeed = 6;
-//        if (castTimeLeft % tickSpeed == 0){
-//            int level = data.getAbilityRank(getAbilityId());
-//            int totalDuration = getCastTime(level);
-//            int count = (totalDuration - castTimeLeft) / tickSpeed;
-//            float baseAmount = level > 1 ? 0.10f : 0.15f;
-//            float scaling = count * baseAmount;
-//            // What to do for each target hit
-//            SpellCast damage = AbilityMeleeDamage.Create(entity, BASE_DAMAGE, DAMAGE_SCALE, scaling);
-//            SpellCast particlePotion = ParticlePotion.Create(entity,
-//                    EnumParticleTypes.SWEEP_ATTACK.getParticleID(),
-//                    ParticleEffects.CIRCLE_MOTION, false,
-//                    new Vec3d(1.0, 1.0, 1.0),
-//                    new Vec3d(0.0, 1.0, 0.0),
-//                    4, 0, 1.0);
-//
-//
-//            AreaEffectBuilder.Create(entity, entity)
-//                    .spellCast(damage, level, getTargetType())
-//                    .spellCast(particlePotion, level, getTargetType())
-//                    .spellCast(SoundPotion.Create(entity, ModSounds.spell_shadow_2, SoundCategory.PLAYERS),
-//                            1, getTargetType())
-//                    .instant()
-//                    .color(16409620).radius(getDistance(level), true)
-//                    .particle(EnumParticleTypes.CRIT)
-//                    .spawn();
-//
-//            Vec3d lookVec = entity.getLookVec();
-//            MKUltra.packetHandler.sendToAllAround(
-//                    new ParticleEffectSpawnPacket(
-//                            EnumParticleTypes.SWEEP_ATTACK.getParticleID(),
-//                            ParticleEffects.SPHERE_MOTION, 16, 4,
-//                            entity.posX, entity.posY + 1.0,
-//                            entity.posZ, 1.0, 1.0, 1.0, 1.5,
-//                            lookVec),
-//                    entity, 50.0f);
-//        }
-//    }
-//
-//    @Override
-//    public void execute(EntityPlayer entity, IPlayerData pData, World theWorld) {
-//        pData.startAbility(this);
-//    }
-//}
+package com.chaosbuffalo.mkultra.abilities.brawler;
+
+import com.chaosbuffalo.mkcore.GameConstants;
+import com.chaosbuffalo.mkcore.abilities.AbilityContext;
+import com.chaosbuffalo.mkcore.abilities.AbilityTargetSelector;
+import com.chaosbuffalo.mkcore.abilities.AbilityTargeting;
+import com.chaosbuffalo.mkcore.abilities.MKAbility;
+import com.chaosbuffalo.mkcore.abilities.ai.conditions.MeleeUseCondition;
+import com.chaosbuffalo.mkcore.core.AbilityType;
+import com.chaosbuffalo.mkcore.core.CastInterruptReason;
+import com.chaosbuffalo.mkcore.core.IMKEntityData;
+import com.chaosbuffalo.mkcore.core.MKAttributes;
+import com.chaosbuffalo.mkcore.effects.AreaEffectBuilder;
+import com.chaosbuffalo.mkcore.effects.MKEffectBuilder;
+import com.chaosbuffalo.mkcore.effects.instant.MKAbilityDamageEffect;
+import com.chaosbuffalo.mkcore.effects.utility.MKParticleEffect;
+import com.chaosbuffalo.mkcore.effects.utility.SoundEffect;
+import com.chaosbuffalo.mkcore.init.CoreDamageTypes;
+import com.chaosbuffalo.mkcore.serialization.attributes.FloatAttribute;
+import com.chaosbuffalo.mkcore.serialization.attributes.ResourceLocationAttribute;
+import com.chaosbuffalo.mkultra.MKUltra;
+import com.chaosbuffalo.mkultra.effects.SeverTendonEffect;
+import com.chaosbuffalo.mkultra.init.ModSounds;
+import com.chaosbuffalo.targeting_api.TargetingContext;
+import com.chaosbuffalo.targeting_api.TargetingContexts;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import javax.annotation.Nullable;
+import java.text.DecimalFormat;
+
+
+public class WhirlwindBlades extends MKAbility {
+    public static final ResourceLocation CAST_PARTICLES = new ResourceLocation(MKUltra.MODID, "whirlwind_blades_pulse");
+    public static WhirlwindBlades INSTANCE = new WhirlwindBlades();
+    protected final ResourceLocationAttribute cast_particles = new ResourceLocationAttribute("cast_particles", CAST_PARTICLES);
+
+    protected final FloatAttribute modifierScaling = new FloatAttribute("modifierScaling", 1.0f);
+    protected final FloatAttribute base = new FloatAttribute("base", 2.0f);
+    protected final FloatAttribute scale = new FloatAttribute("scale", 1.0f);
+    protected final FloatAttribute perTick = new FloatAttribute("perTick", 0.15f);
+
+    private WhirlwindBlades() {
+        super(MKUltra.MODID, "ability.whirlwind_blades");
+        addAttributes(cast_particles, base, scale, modifierScaling, perTick);
+        setCastTime(GameConstants.TICKS_PER_SECOND * 3);
+        setCooldownSeconds(20);
+        setManaCost(6);
+        addSkillAttribute(MKAttributes.PANKRATION);
+        setUseCondition(new MeleeUseCondition(this));
+    }
+
+    @Override
+    public AbilityType getType() {
+        return AbilityType.Ultimate;
+    }
+
+    @Override
+    public boolean canApplyCastingSpeedModifier() {
+        return false;
+    }
+
+    @Override
+    public boolean isInterruptedBy(IMKEntityData targetData, CastInterruptReason reason) {
+        return false;
+    }
+
+    @Override
+    public AbilityTargetSelector getTargetSelector() {
+        return AbilityTargeting.PBAOE;
+    }
+
+    @Override
+    protected ITextComponent getAbilityDescription(IMKEntityData entityData) {
+        float level = getSkillLevel(entityData.getEntity(), MKAttributes.PANKRATION);
+        ITextComponent baseDamage = getDamageDescription(entityData,
+                CoreDamageTypes.MeleeDamage, base.value(), scale.value(), level, 0.0f);
+        float periodSeconds = 6.0f / GameConstants.TICKS_PER_SECOND;
+        int castSeconds = getCastTime(entityData) / GameConstants.TICKS_PER_SECOND;
+        int numberOfCasts = Math.round(castSeconds / periodSeconds);
+        ITextComponent maxDamage = getDamageDescription(entityData,
+                CoreDamageTypes.MeleeDamage, base.value(), scale.value(), level,
+                modifierScaling.value() * numberOfCasts * perTick.value());
+        return new TranslationTextComponent(getDescriptionTranslationKey(), NUMBER_FORMATTER.format(periodSeconds),INTEGER_FORMATTER.format(castSeconds),
+                PERCENT_FORMATTER.format(perTick.value()), baseDamage, maxDamage);
+    }
+
+    @Override
+    public TargetingContext getTargetContext() {
+        return TargetingContexts.ENEMY;
+    }
+
+    @Override
+    public float getDistance(LivingEntity entity) {
+        return getMeleeReach(entity);
+    }
+
+        @Override
+    public SoundEvent getCastingSoundEvent() {
+        return ModSounds.spell_whirlwind_1;
+    }
+
+    @Nullable
+    @Override
+    public SoundEvent getSpellCompleteSoundEvent() {
+        return null;
+    }
+
+    @Override
+    public void continueCast(LivingEntity castingEntity, IMKEntityData casterData, int castTimeLeft, AbilityContext context) {
+        super.continueCast(castingEntity, casterData, castTimeLeft, context);
+        int tickSpeed = 6;
+        if (castTimeLeft % tickSpeed == 0) {
+            float level = getSkillLevel(castingEntity, MKAttributes.PANKRATION);
+            int totalDuration = getCastTime(casterData);
+            int count = (totalDuration - castTimeLeft) / tickSpeed;
+            float baseAmount = perTick.value();
+            float scaling = count * baseAmount;
+
+            MKEffectBuilder<?> damage = MKAbilityDamageEffect.from(castingEntity, CoreDamageTypes.MeleeDamage,
+                    base.value(), scale.value(), modifierScaling.value() * scaling)
+                    .ability(this)
+                    .skillLevel(level);
+            MKEffectBuilder<?> particles = MKParticleEffect.from(castingEntity,
+                    cast_particles.getValue(), true, new Vector3d(0.0, 1.0, 0.0))
+                    .ability(this);
+            MKEffectBuilder<?> sound = SoundEffect.from(castingEntity, ModSounds.spell_shadow_2,castingEntity.getSoundCategory())
+                    .ability(this);
+
+            AreaEffectBuilder.createOnCaster(castingEntity)
+                    .effect(damage, getTargetContext())
+                    .effect(particles, TargetingContexts.SELF)
+                    .effect(sound, getTargetContext())
+                    .instant()
+                    .color(16409620)
+                    .radius(getDistance(castingEntity), true)
+                    .disableParticle()
+                    .spawn();
+        }
+    }
+
+    @SuppressWarnings("unused")
+    @Mod.EventBusSubscriber(modid = MKUltra.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    private static class RegisterMe {
+        @SubscribeEvent
+        public static void register(RegistryEvent.Register<MKAbility> event) {
+            event.getRegistry().register(INSTANCE);
+        }
+    }
+}
